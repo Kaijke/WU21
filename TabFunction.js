11e3ad364a57656688c4c261bd7e4d935e6708ec
Kaijke <oxxiden@gmail.com>
2022-10-08 17:59:21 +0200

    0.2.1

--- TabFunction.js
+++ TabFunction.js
@@ -1,3 +1,22 @@
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+/* REFERENCE
+
 function openCity(evt, cityName) {
     // Declare all variables
     var i, tabcontent, tablinks;
@@ -19,3 +38,5 @@ function openCity(evt, cityName) {
     evt.currentTarget.className += " active";
 
 }
+
+*/ 
